0b679017dabe42182b9545572f86b4346a13a13a
Riko <riko.fluchel@gmail.com>
2025-08-28 07:38:23 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -46,6 +46,35 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+        data = request.get_json()
+        for attr, value in data.items():
+            if attr == "is_in_stock":
+                if isinstance(value, str):
+                    value = value.lower() == "true"
+                
+            setattr(plant, attr, value)
+
+        db.session.add(plant)
+        db.session.commit()
+
+        response_dict = jsonify(plant.to_dict())
+
+        response = make_response(response_dict, 200)
+
+        return response
+    
+    def delete(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        response_dict = {"message": "Plant successfully deleted."}
+        response = make_response(jsonify(response_dict), 204)
+        return response
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
